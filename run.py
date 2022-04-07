117342641a324d0feee257f0bea1e939fb42738a
Ben Jim <jimenezbenjamin@yahoo.com>
2022-04-07 14:10:26 +0000

    add the board

--- run.py
+++ run.py
@@ -3,9 +3,15 @@ import random
 if __name__ == "__main__":
     print('Battleship')
     
+#initializing board
+board = []
 
-def board():
-  pass
+for x in range(5):
+    board.append(["O"] * 5)
+
+def print_board(board):
+    for row in board:
+        print (" ".join(row))
 
 
 def players_choice():
@@ -13,54 +19,59 @@ def players_choice():
     your_choice_column = input('Choose your column: ')
     row_choice = int(your_choice_row)
     column_choice = int(your_choice_column)
-    your_coordinates = column_choice, row_choice 
+    your_coordinates = row_choice, column_choice
     print(your_coordinates)
     return your_coordinates
     
 
 def computers_choice():
-    comp_choice_row = random.randint(0,8)
-    comp_choice_column = random.randint(0,8)
+    comp_choice_row = random.randint(1,5)
+    comp_choice_column = random.randint(1,5)
     comp_coordinates = comp_choice_row, comp_choice_column
-    # for comp_coordinate in comp_coordinates
-    print(comp_coordinate)
+    for comp_coordinate in comp_coordinates:
+        print(comp_coordinate)
     return comp_coordinates
 
 def players_ship():
-    players_ship_row = random.randint(1,8) 
-    players_ship_column = random.randint(1,8) 
+    players_ship_row = random.randint(1,5) 
+    players_ship_column = random.randint(1,5) 
     players_ship_coordinates = players_ship_row, players_ship_column
     print(players_ship_coordinates)
     return players_ship_coordinates
 
 def computer_ship():
-    computer_ship_row = random.randint(1,8)
-    computer_ship_column = random.randint(1,8)
-    computer_ship_coordinates = computer_ship_row, computer_ship_column
+    # computer_ship_row = random.randint(1,8)
+    # computer_ship_column = random.randint(1,8)
+    # computer_ship_coordinates = computer_ship_row, computer_ship_column
+    computer_ship_coordinates = 2, 3
     print(computer_ship_coordinates)
     return computer_ship_coordinates
 
 
 
 
-players_shot = players_choice()  
-computer_coordinate = computer_ship()
-computer_shot = computers_choice()
-players_coordinate = players_ship()
 
-def players_turn(players_shot, computer_coordinate):
+def players_turn(players_shot, computer_coordinates):
     score = 0
     score_max = 5
     while (score < 5):
     
-        if players_shot == computer_coordinate:
+        if players_shot == computer_coordinates:
             score += 1
             print("one ship Down!")
+        elif score == 5:
+            print('you Win!')
+            break
         else:
             print("miss!!")
-            break
+print_board(board)
+players_shot = players_choice()  
+computer_coordinates = computer_ship()
+computer_shot = computers_choice()
+players_coordinate = players_ship()
+players_turn(players_shot, computer_coordinates)
 
-players_turn(players_shot, computer_coordinate)
+
 
 
 # def random_coordinate():
@@ -70,3 +81,4 @@ players_turn(players_shot, computer_coordinate)
 
 # board()
 # random_spot = random_coordinate()
+
